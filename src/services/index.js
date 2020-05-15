32443b04e19d771ce19e1af94aa18e8d24aec079
Dreamycqy <1316152943@qq.com>
2020-05-15 14:01:51 +0800

    fix

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(body, rSymbol) {
   return request.post({
-    url: '/api/svc/ForeignLogin/login',
+    url: '/foreign-news/api/svc/ForeignLogin/login',
     data: body,
     rSymbol,
   })
@@ -10,7 +10,7 @@ export function login(body, rSymbol) {
 
 export function search(body, rSymbol) {
   return request.post({
-    url: '/api/svc/Foreign/queryNewsV2',
+    url: '/foreign-news/api/svc/Foreign/queryNewsV2',
     data: body,
     rSymbol,
   })
@@ -18,7 +18,7 @@ export function search(body, rSymbol) {
 
 export function subQueryNews(body, rSymbol) {
   return request.post({
-    url: '/api/svc/Foreign/subQueryNewsV2',
+    url: '/foreign-news/api/svc/Foreign/subQueryNewsV2',
     data: body,
     rSymbol,
   })
@@ -26,7 +26,7 @@ export function subQueryNews(body, rSymbol) {
 
 export function getFilterKeyword(body, rSymbol) {
   return request.get({
-    url: '/api/svc/Foreign/getFilterKeyword',
+    url: '/foreign-news/api/svc/Foreign/getFilterKeyword',
     data: body,
     rSymbol,
   })
@@ -34,7 +34,7 @@ export function getFilterKeyword(body, rSymbol) {
 
 export function updateFilterKeyword(body, rSymbol) {
   return request.post({
-    url: '/api/svc/Foreign/updateFilterKeyword',
+    url: '/foreign-news/api/svc/Foreign/updateFilterKeyword',
     data: body,
     rSymbol,
   })
